7a2cc65571859254af9a6709e060fc42dcf8e9a3
Djamel <Djamel@mail.com>
2024-06-17 15:06:31 +0200

    first commit

---
+++ playwright.config.ts
@@ -0,0 +1,108 @@
+import { defineConfig, devices } from '@playwright/test';
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// require('dotenv').config();
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  testDir: './tests',
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'html',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    /* Base URL to use in actions like `await page.goto('/')`. */
+     baseURL: 'https://www.jettours.com/',
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+
+    {
+      name: 'iphone 14 pro max',
+      use: { ...devices["iPhone 14 Pro Max"] },
+    },
+
+    {
+      name: 'iPad 11 pro',
+      use: { ...devices["iPad Pro 11"] },
+    },
+    {
+      name: 'Samsung tab 4',
+      use: { ...devices["Galaxy Tab S4"] },
+    },
+
+    {
+      name: 'Samsung s9+',
+      use: { ...devices["Galaxy S9+"] },
+    },
+
+
+    {
+      name: 'Desktop Chrome',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'Desktop Firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'Desktop Safari',
+      use: { ...devices['Desktop Safari'] },
+    },
+    {
+      name: 'Mobile Chrome',
+      use: { ...devices['Pixel 5'] },
+    },
+    {
+      name: 'Mobile Safari',
+      use: { ...devices['iPhone 12'] },
+    },
+    {
+      name: 'Microsoft Edge',
+      use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    },
+    {
+      name: 'Google Chrome',
+      use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    },
+
+
+    {
+      name: 'staging',
+      use: {
+        baseURL: 'staging.example.com',
+      },
+      retries: 2,
+    },
+    {
+      name: 'production',
+      use: {
+        baseURL: 'production.example.com',
+      },
+      retries: 0,
+    },
+  ],
+
+  /* Run your local dev server before starting the tests */
+  // webServer: {
+  //   command: 'npm run start',
+  //   url: 'http://127.0.0.1:3000',
+  //   reuseExistingServer: !process.env.CI,
+  // },
+});
---
+++ tests/Ipad/NavigationIpad.spec.ts
@@ -0,0 +1,34 @@
+import { test, expect } from '@playwright/test';
+import { describe } from 'node:test';
+
+describe("Tests moblies",()=>{
+  test('test', async ({ page }) => {
+    await page.goto('/');
+    await page.getByText('Accepter et fermer').click();
+    await page.locator('#header > a.btn-menu.pull-left.visible-sm > button').click();                  
+    await page.locator('#mm-1 > ul > li:nth-child(1) > a').click(); 
+    await page.locator('#mm-2 > ul > li:nth-child(1) > a').click();
+    await page.getByRole('link', { name: 'Tunisie', exact: true }).click();
+    await page.locator('#header > a.btn-menu.pull-left.visible-sm > button').click();
+    await page.locator('#mm-1 > ul > li:nth-child(2) > a').click();
+    await page.locator('#mm-6 > ul > li:nth-child(2) > a').click();
+    await page.getByRole('link', { name: 'Andalousie' }).click();
+    await page.locator('#header > a.btn-menu.pull-left.visible-sm > button').click();
+    await page.waitForSelector("#mm-1 > ul > li:nth-child(3) > a");
+    await page.locator('#mm-1 > ul > li:nth-child(3) > a').click();
+    await page.waitForSelector("#mm-11 > div > a.mm-btn.mm-prev"); 
+    await page.locator('#mm-11 > div > a.mm-btn.mm-prev').click();  
+    await page.locator('#mm-1 > ul > li:nth-child(4) > a').click();
+    await page.waitForSelector("#mm-15 > div > a.mm-btn.mm-prev");
+    await page.locator('#mm-15 > div > a.mm-btn.mm-prev').click();
+    await page.waitForSelector("#mm-1 > ul > li.icon.dropdown.expand > a");
+    await page.locator('#mm-1 > ul > li.icon.dropdown.expand > a').click();
+    await page.waitForSelector("#mm-19 > div > a.mm-btn.mm-prev");
+    await page.locator('#mm-19 > div > a.mm-btn.mm-prev').click();
+    await expect(page.getByRole('button', { name: 'Circuits ' })).toBeVisible();
+    await expect(page.locator('h1')).toContainText('Kappa Club, l’expérience du club ouvert sur la découverte');
+    await expect(page.getByPlaceholder('JJ/MM/AAAA')).toHaveValue('Date de départ');
+    page.close();
+  });
+})
+
